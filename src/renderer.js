9c5b3da3e1cab52179aff20e1ba1cf0d326ee024
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-04 00:42:09 +0300

    updated

--- src/renderer.js
+++ src/renderer.js
@@ -4,5 +4,5 @@ import App from "./app.jsx";
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.body);
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
